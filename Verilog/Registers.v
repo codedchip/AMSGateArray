15ba555e07bfb5731459d38a1966868cf7d1ce69
Darren Johnstone <darren.johnstone@aggreko.co.uk>
2022-02-22 18:21:38 +0000

    Simplifying registers and making synchronous.

--- Verilog/Registers.v
+++ Verilog/Registers.v
@@ -23,7 +23,7 @@
 //  Based on 40010-simplified_V03.pdf by Gerald 
 //  (https://www.cpcwiki.eu/forum/amstrad-cpc-hardware/gate-array-decapped!) 
 //  
-//  Partially based on the Amstrad MiSTer core by Gyorgy Szombathelyi
+//  Partially based on  the Amstrad MiSTer core by Gyorgy Szombathelyi
 //  https://github.com/MiSTer-devel/Amstrad_MiSTer/tree/master/rtl/GA40010
 // ===============================================================================
 module Registers(input CLK_n,
@@ -35,73 +35,64 @@ module Registers(input CLK_n,
                  input S0,
                  input S7,
                  input[7 : 0] D,
-                 output[4 : 0] BORDER,
-                 output IRQ_RESET,
-                 output HROMEN,
-                 output LROMEN,
-                 output[1 : 0] MODE,
-                 output[15 : 0] INKR0,
-                 output[15 : 0] INKR1,
-                 output[15 : 0] INKR2,
-                 output[15 : 0] INKR3,
-                 output[15 : 0] INKR4);
+                 output reg [4 : 0] BORDER,
+                 output reg IRQ_RESET,
+                 output reg HROMEN,
+                 output reg LROMEN,
+                 output reg [1 : 0] MODE,
+                 output reg [15 : 0] INKR0,
+                 output reg [15 : 0] INKR1,
+                 output reg [15 : 0] INKR2,
+                 output reg [15 : 0] INKR3,
+                 output reg [15 : 0] INKR4);
+
   reg[4 : 0] inksel;
-  wire reg_sel = M1_n & A14 & ~A15 & ~IORQ_n & S0 & S7; // u401
-  wire inksel_en = reg_sel & ~D[7] & ~D[6]; // u402
-  wire border_en = reg_sel & ~D[7] & D[6] & inksel[4]; // u408
-  wire reg_en = reg_sel & D[7] & ~D[6]; // u414
-  // u420 
-  // on the schematic the ~inksel[4] condition is missing, but this would mean border 
-  // and ink potentially being selected at the same time
-  wire ink_en = reg_sel & ~D[7] & D[6] & ~inksel[4];
-  assign IRQ_RESET = reg_en & D[4];
-  reg _hromen,
-      _lromen,
-      _mode0,
-      _mode1;
-  reg[4 : 0] _border;
-  reg[15 : 0] _inkr0;
-  reg[15 : 0] _inkr1;
-  reg[15 : 0] _inkr2;
-  reg[15 : 0] _inkr3;
-  reg[15 : 0] _inkr4;
+  reg ink_en,
+      reg_sel,
+      inksel_en,
+      border_en,
+      reg_en;
+
   // Move to clock domain
   always
     @(posedge CLK_n)
       begin
+        // wires
+        reg_sel = M1_n & A14 & ~A15 & ~IORQ_n & S0 & S7; // u401
+        inksel_en = reg_sel & ~D[7] & ~D[6]; // u402
+        border_en = reg_sel & ~D[7] & D[6] & inksel[4]; // u408
+        reg_en = reg_sel & D[7] & ~D[6]; // u414
+
+        // on the schematic the ~inksel[4] condition is missing, but this would mean border 
+        // and ink potentially being selected at the same time
+        ink_en = reg_sel & ~D[7] & D[6] & ~inksel[4]; // u420
+
         // inksel branch
         if (inksel_en)
           inksel <= D[4 : 0];
         // border branch
         if (RESET)
-          _border <= 5'b10000;
+          BORDER <= 5'b10000;
         else
           if (border_en)
-            _border <= D[4 : 0];
+            BORDER <= D[4 : 0];
         // register branch
         if (RESET)
-          { _hromen, _lromen, _mode1, _mode0 } <= 0;
+          { HROMEN, LROMEN, MODE[1], MODE[0] } <= 0;
         else
           if (reg_en)
-            { _hromen, _lromen, _mode1, _mode0 } <= D[3 : 0];
+            { HROMEN, LROMEN, MODE[1], MODE[0] } <= D[3 : 0];
         // ink branch
         if (ink_en)
           begin
-            _inkr0[inksel[3 : 0]] <= D[0];
-            _inkr1[inksel[3 : 0]] <= D[1];
-            _inkr2[inksel[3 : 0]] <= D[2];
-            _inkr3[inksel[3 : 0]] <= D[3];
-            _inkr4[inksel[3 : 0]] <= D[4];
+            INKR0[inksel[3 : 0]] <= D[0];
+            INKR1[inksel[3 : 0]] <= D[1];
+            INKR2[inksel[3 : 0]] <= D[2];
+            INKR3[inksel[3 : 0]] <= D[3];
+            INKR4[inksel[3 : 0]] <= D[4];
           end
+
+          IRQ_RESET <= reg_en & D[4];
       end
-  assign INKR0 = _inkr0;
-  assign INKR1 = _inkr1;
-  assign INKR2 = _inkr2;
-  assign INKR3 = _inkr3;
-  assign INKR4 = _inkr4;
-  assign BORDER = _border;
-  assign HROMEN = _hromen;
-  assign LROMEN = _lromen;
-  assign MODE[0] = _mode0;
-  assign MODE[1] = _mode1;
+
 endmodule
